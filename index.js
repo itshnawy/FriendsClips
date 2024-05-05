e5fcd881b35ed97713e00063501f85522b8c21d6
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-05-05 22:29:36 +0300

    a simple design and script

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// simple of the code vars
+let videoURL = 'https://www.youtube.com/embed/RjpvuPAzJUw';
+let startTime = '?start=10';
+let endTime = '&end=66';
+let addToUrl = '&controls=0&cc_load_policy=1&cc_lang_pref=ar';
+let clipURL = videoURL + startTime + endTime + addToUrl;
 
+
+document.getElementById('clip').setAttribute('src', clipURL);
+document.getElementById('saep').innerHTML = "Season 3 EP 2: The One Where No One's Ready"
